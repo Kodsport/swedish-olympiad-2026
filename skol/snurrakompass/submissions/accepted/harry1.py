b583a4b973416f02c93d5bc8a3a82ba8385c5715
Raunak Redkar <raunak.redkar@gmail.com>
2025-09-20 18:15:28 +0200

    snurrakompass: adding more accepted solutions

---
+++ skol/snurrakompass/submissions/accepted/harry1.py
@@ -0,0 +1,3 @@
+import math
+a,b = map(int,input().split())
+print(360//math.gcd(a,math.gcd(b,360)))
---
+++ skol/snurrakompass/submissions/accepted/harry2.py
@@ -0,0 +1,13 @@
+vis = [0]*360
+vis[0] = 1
+a,b = map(int,input().split())
+BFS = [0]
+for cur in BFS:
+  for dx in [a,b,-a,-b]:
+    nei = cur+dx
+    nei %= 360
+    if vis[nei]: continue
+    vis[nei] = 1
+    BFS.append(nei)
+
+print(sum(vis))
