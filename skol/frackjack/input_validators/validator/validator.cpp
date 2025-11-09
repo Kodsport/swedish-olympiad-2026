3c6652f1fbcf8adbf655f79ac64554263d3b9700
hairez <harryzhang2004@hotmail.com>
2025-11-09 05:37:16 +0100

    [frackjack] Update input validator

--- skol/frackjack/input_validators/validator/validator.cpp
+++ skol/frackjack/input_validators/validator/validator.cpp
@@ -7,11 +7,8 @@ using namespace std;
 void run() {
 	int maxa=Arg("maxa",30);
 	int maxb=Arg("maxb",30);
-	int a = Int(0,30); Endl();
-	int b = Int(0,30); Endl();
-
-	assert(a<=30);
-	assert(b<=30);
+	int a = Int(0,maxa); Endl();
+	int b = Int(0,maxb); Endl();
 
 	Eof();
 }
