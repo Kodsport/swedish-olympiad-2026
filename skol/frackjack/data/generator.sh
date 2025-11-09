d7142b17dbe9d1cd2bb57f79254425d4e609a95e
Ruiming Zhang <rzhang8776@gmail.com>
2025-11-09 08:59:43 +0100

    changed frackjack

--- skol/frackjack/data/generator.sh
+++ skol/frackjack/data/generator.sh
@@ -5,9 +5,9 @@ use_solution ruiming.cpp
 
 compile gen_rand.py
 
-#samplegroup
-#sample_manual 1
-#sample_manual 2
+samplegroup
+sample 1
+sample 2
 
 # Intentionally do not merge groups to see what solutions actually get during skolkval
 
@@ -16,11 +16,18 @@ limits maxa=30 maxb=30
 #tc_manual ../manual/secret_${i}.in
 tc 1
 tc 2
-tc 3
-tc 4
-tc 5
-tc 6
-tc 7
-tc 8
-tc 9
-tc 10
+tc_manual ../manual/secret_1.in
+tc_manual ../manual/secret_2.in
+tc_manual ../manual/secret_3.in
+tc_manual ../manual/secret_4.in
+tc_manual ../manual/secret_5.in
+tc g-1 gen_rand a=5 b=5
+tc g-2 gen_rand a=5 b=5
+tc g-3 gen_rand a=10 b=10
+tc g-4 gen_rand a=10 b=10
+tc g-5 gen_rand a=10 b=10
+tc g-6 gen_rand a=30 b=30
+tc g-7 gen_rand a=30 b=30
+tc g-8 gen_rand a=30 b=30
+tc g-9 gen_rand a=30 b=30
+tc g-10 gen_rand a=30 b=30
