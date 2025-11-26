bb7098b2d3b1763d3c4e48bf3800922ed215e04a
Raunak Redkar <raunak.redkar@gmail.com>
2025-11-26 14:40:18 +0100

    erm

---
+++ online/lojtnantslotto/data/generator.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+PPATH=$(realpath ..)
+. ../../../testdata_tools/gen.sh
+
---
+++ online/lojtnantslotto/input_validators/validator/validator.cpp
@@ -0,0 +1,16 @@
+#include "validator.h"
+
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(i, a) for(auto& i : a)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+
+void run() {
+}
+
