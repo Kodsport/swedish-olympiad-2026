73ee6e9cd47d04d92c546c3445af725252eb9c5d
nigus444 <nilsgust@kth.se>
2025-11-27 12:42:58 +0100

    [bytadelare] more test cases

--- online/bytadelare/data/gen.py
+++ online/bytadelare/data/gen.py
@@ -20,7 +20,7 @@ def is_prime(x):
             return True
     return True
 
-def valid_shuffle(A):
+def valid_shuffle(A, rev=False):
     A2 = []
     for i in range(len(A)):
         A2.append((A[i], i))
@@ -41,12 +41,15 @@ def valid_shuffle(A):
                 if seen[a] == 0:
                     seen[a] = 1
                     component.append(a)
-                    for i2 in range(0, n):
+                    for i2 in range(0, len(A)):
                         if seen[i2] == 0 and i2 != a and (A[i2]%A[a] == 0 or A[a]%A[i2] == 0):
                             Q.append(i2)
                 j += 1
             comp2 = component.copy()
-            random.shuffle(comp2)
+            if rev:
+                comp2 = comp2[::-1]
+            else:
+                random.shuffle(comp2)
             for j in range(len(comp2)):
                 res[goal[comp2[j]]] = A[component[j]]
     return res
@@ -59,6 +62,7 @@ maxx = int(cmdlinearg('maxx', 10**6))
 a1 = int(cmdlinearg('a1', -1))
 u = int(cmdlinearg('u', 1000))
 mode = cmdlinearg('mode', 'random')
+noise = int(cmdlinearg('noise', 0))
 
 small_primes = []
 for i in range(2, int(maxx**0.5)+1):
@@ -86,8 +90,87 @@ elif mode == "prime_power":
         A.append(prime_powers[random.randrange(len(prime_powers))])
     A = valid_shuffle(A)
 
+elif mode == "prime_forest":
+    comps = int(cmdlinearg("comps", 1))
+    branch = int(cmdlinearg("branch", n))
+    p_choose = (n+comps) // 2
+    p_choose = min(p_choose, len(small_primes))
+    primes = random.sample(small_primes, p_choose)
+
+    for i in range(len(primes)):
+        A.append(primes[i])
+        if i >= comps:
+            start = max(0, i-branch)
+            par = random.randrange(start, i)
+            A.append(primes[par]*primes[i])
+    
+    while len(A) < n:
+        a = random.randrange(len(A))
+        A.append(A[a])
+    
+    A = valid_shuffle(A)
+
+elif mode == "prime_line":
+    primes = []
+    p = 0
+    num = maxx
+    while True:
+        if p >= len(small_primes):
+            break
+        while num > 1 and ((not is_prime(num)) or num*small_primes[p] > maxx):
+            num -= 1
+        if num <= small_primes[p]:
+            break
+        primes.append(num)
+        primes.append(small_primes[p])
+        p += 1
+        num -= 1
+
+    for i in range(len(primes)-1):
+        A.append(primes[i])
+        A.append(primes[i]*primes[i+1])
+    A.append(primes[-1])
+
+    if len(A) > n:
+        A = A[::n]
+
+    
+    for i in range(n-len(A)):
+        if i%2 == 0:
+            A.append(primes[0])
+        else:
+            A.append(primes[-1])
+    
+
+    A = valid_shuffle(A, True)        
+        
+
+
+elif mode == "one_edge":
+    assert u >= 2
+    diff[0] = random.randint(2, maxx // 2)
+    diff[1] = 2*diff[0]
+    if u >= 3:
+        diff[2] = random.randint(2,maxx)
+    for _ in range(n):
+        A.append(diff[random.randrange(len(diff))])
+    A = valid_shuffle(A)
+
+elif mode == "two_edge":
+    assert u >= 3
+    diff[0] = random.randint(2, maxx // 3)
+    diff[1] = 2*diff[0]
+    diff[2] = 3*diff[0]
+    for _ in range(n):
+        A.append(diff[random.randrange(len(diff))])
+
 if a1 != -1:
     A[0] = a1
+
+
+for _ in range(noise):
+    i,j = random.randrange(len(A)), random.randrange(len(A))
+    A[i],A[j] = A[j],A[i] 
 
 print(len(A),x)
 print(*A)
--- online/bytadelare/data/generator.sh
+++ online/bytadelare/data/generator.sh
@@ -28,17 +28,22 @@ tc g1-5 gen n=10 maxx=10
 tc g1-6 gen n=10 maxx=5
 tc g1-prime-power-1 gen n=10 maxx=10 mode=prime_power
 tc g1-prime-power-2 gen n=10 mode=prime_power
+tc g1-prime-forest-1 gen n=10 mode=prime_forest comps=3
 
 group group2 10
 limits max_unique=2
 tc g2-1 gen n=$MAXN u=2 maxx=2
 tc g2-2 gen n=$MAXN u=2 maxx=4
 tc g2-3 gen n=$MAXN u=1
+tc g2-4 gen n=$MAXN u=2 mode=one_edge
 
 group group3 13
 limits max_unique=3
 include_group group2
 tc g3-1 gen n=$MAXN u=3 maxx=4
+tc g3-2 gen n=$MAXN u=3 mode=one_edge
+tc g3-3 gen n=$MAXN u=3 mode=one_edge noise=10
+tc g3-4 gen n=$MAXN u=3 mode=two_edge
 
 group group4 30
 limits maxx=0
@@ -49,6 +54,11 @@ tc g4-3 gen n=$MAXN x=0 maxx=555
 tc g4-4 gen n=$MAXN x=0 maxx=2000
 tc g4-5 gen n=$MAXN x=0 maxx=10000
 tc g4-prime-power-1 gen n=$MAXN x=0 mode=prime_power
+tc g4-prime-power-2 gen n=$MAXN x=0 mode=prime_power noise=10
+tc g4-prime-forest-1 gen n=$MAXN x=0 mode=prime_forest comps=100
+tc g4-prime-forest-2 gen n=$MAXN x=0 mode=prime_forest noise=10 comps=100
+tc g4-prime-line-1 gen n=$MAXN  x=0 mode=prime_line
+tc g4-prime-line-2 gen n=$MAXN  x=0 mode=prime_line noise=10
 
 group group5 12
 limits a1=1
@@ -65,4 +75,12 @@ tc g6-1 gen n=$MAXN maxx=111
 tc g6-2 gen n=$MAXN maxx=111111
 tc g6-prime-power-1 gen n=$MAXN maxx=1000 mode=prime_power
 tc g6-prime-power-2 gen n=$MAXN mode=prime_power
+tc g6-prime-forest-1 gen n=$MAXN mode=prime_forest comps=1
+tc g6-prime-forest-2 gen n=$MAXN mode=prime_forest comps=10
+tc g6-prime-forest-3 gen n=$MAXN mode=prime_forest comps=30
+tc g6-prime-forest-4 gen n=$MAXN mode=prime_forest comps=100
+tc g6-prime-forest-5 gen n=$MAXN mode=prime_forest comps=999
+tc g6-prime-forest-6 gen n=$MAXN mode=prime_forest comps=1 branch=1
+tc g6-prime-forest-7 gen n=$MAXN mode=prime_forest comps=2 branch=6
+tc g6-prime-line-1 gen n=$MAXN  mode=prime_line
 
