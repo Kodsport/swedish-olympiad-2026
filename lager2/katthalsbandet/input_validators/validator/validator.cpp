f75ab9de515c3fb7135ae415665b910c49966b88
hairez <harryzhang2004@hotmail.com>
2026-03-28 02:26:34 +0100

    [katthalsbandet] add check.md fixes and jpg

--- lager2/katthalsbandet/input_validators/validator/validator.cpp
+++ lager2/katthalsbandet/input_validators/validator/validator.cpp
@@ -3,12 +3,13 @@
 void run() {
     int n = Int(1, Arg("maxn", 500000));
     Space();
-    int a = Int(1, 1e9);
+    int a = Int(Arg("mina", 1), Arg("maxa", 1000000000));
     Space();
-    int b = Int(Arg("minb", 1), Arg("maxb", 1000000000));
+    int b = Int(Arg("minb", 0), Arg("maxb", 1000000000));
     Endl();
 
     string row = Line();
+    assert(row.size() == (size_t)n);
     int left = 0, right = 0;
     for (int i = 0; i < n; i++) {
         char c = row[i];
